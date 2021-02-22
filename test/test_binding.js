9f8bf8f4b144cd1768757275b5f7a8bd550a3df2
Sebastian Greco <sebastian.greco@bothmedia.com>
2021-02-22 10:12:21 +0100

    updated test to see if there are leaks

--- test/test_binding.js
+++ test/test_binding.js
@@ -10,8 +10,13 @@ function testBasic()
     assert(instance.greet, "The expected method is not defined");
     //assert.strictEqual(instance.greet("kermit"), "mr-yeoman", "Unexpected value returned");
     
-    console.log(instance.grab())
-    console.log(instance.grab())
+    const start = Date.now();
+    const amount = 1500;
+    for (let i = amount; i > 0; i--) {
+        ((instance).grab());
+    }
+    const took = Date.now() - start;
+    console.log(`Took ${took} ms -  AVG ${took/amount}`)
 }
 
 function testInvalidParams()
